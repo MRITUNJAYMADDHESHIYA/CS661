f16aa7f36512abf703982eb4659b9b2a8a9fe250
Mritunjay Maddhesiya <mritunjay112002@gmail.com>
2025-04-29 11:43:44 +0530

    modification

--- static/wordcloud-chart.js
+++
@@ -1,6 +0,0 @@
-function renderWordCloud(imageData) {
-    const container = document.getElementById('wordcloud');
-    container.innerHTML = imageData
-        ? `<img src="${imageData}" style="max-width:100%;"/>`
-        : '<span style="color:#bbb;">No Data</span>';
-}
